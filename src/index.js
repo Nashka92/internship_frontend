add4829db63b733bbfd859abf0b8956c35867cf6
anna raach <annagraph92@gmail.com>
2022-06-19 14:56:56 +0200

    projet react, api etc

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import ReactTooltip from "react-tooltip";
+import Axios from "axios";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./styles.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import MapChart from "./MapChart";
+
+function App() {
+  const [appState, setAppState] = useState("hhh");
+  const [content, setContent] = useState("");  
+
+  useEffect(() => {
+    const apiUrl = 'https://localhost:8000/api/project';
+    Axios.get(apiUrl).then((resp) => {
+      const allPersons = resp.data;
+      setAppState({allPersons});
+      
+      console.log(resp.data);
+    });
+  }, [setAppState]);
+  
+  const data = Array.from(appState);
+  console.log(appState);
+  return (
+    <div>
+      <MapChart setTooltipContent={setContent} />
+      <ReactTooltip>{content}</ReactTooltip>
+    
+     <ul> 
+      {data.map(monObjet => <li>Mon {monObjet.id} est de couleur {monObjet.prenom}</li>)}
+    </ul>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
